8aa55bec952efe2279e3fa3a2dec6da6e2bd1b66
umran23 <umran.bac@gmail.com>
2021-04-06 09:25:36 +0300

    Creating Server Program

---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+
+sock=socket.socket()
+
+sock.bind(('127.0.0.1',3000))
+sock.listen(0)
+
+conn, addr =sock.accept()
+print(addr)
+
+msg = ''
+
+while True:
+	data=conn.recv(1024)
+	if not data:
+		break
+	msg += data.decode()
+	conn.send(data)
+
+print(msg)
+conn.close()
